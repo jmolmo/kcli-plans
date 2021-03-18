dc6e5589ba860835a86756a8f2eaddf2f4c0c188
Juan Miguel Olmo Martínez <jolmomar@redhat.com>
2021-03-18 12:58:35 +0100

    Ceph: Fix nodes addition to the cluster in the cephadm plan
    Signed-off-by: Juan Miguel Olmo Martínez <jolmomar@redhat.com>

--- ceph/bootstrap-cluster.sh
+++ ceph/bootstrap-cluster.sh
@@ -8,7 +8,7 @@ mon_ip=$(ifconfig eth0  | grep 'inet ' | awk '{ print $2}')
 cephadm bootstrap --mon-ip $mon_ip --allow-fqdn-hostname --initial-dashboard-password {{ admin_password }}
 fsid=$(cat /etc/ceph/ceph.conf | grep fsid | awk '{ print $3}')
 {% for number in range(1, nodes) %}
-  ssh-copy-id -f -i /etc/ceph/ceph.pub  -o StrictHostKeyChecking=no root@{{ prefix }}-node-0{{ number }}
+  ssh-copy-id -f -i /etc/ceph/ceph.pub  -o StrictHostKeyChecking=no root@{{ prefix }}-node-0{{ number }}.{{domain}}
   cephadm shell --fsid $fsid -c /etc/ceph/ceph.conf -k /etc/ceph/ceph.client.admin.keyring ceph orch host add {{ prefix }}-node-0{{ number }}.{{domain}}
 {% endfor %}
 cephadm shell --fsid $fsid -c /etc/ceph/ceph.conf -k /etc/ceph/ceph.client.admin.keyring ceph orch apply osd --all-available-devices
